511085b6d000f15cd784593fefa55bbd3ca5d643
Enrico R. Crema <enrico.crema@gmai.com>
2021-11-02 21:26:13 +0000

    Reads KDE_model js output in R

--- src/kde_read.R
+++ src/kde_read.R
@@ -1,10 +1,9 @@
-x <- readLines("~/Downloads/kde_test.js")
-
 read_kde <- function(x,sigma=1)
 {
 	x <- x[grep("kde_mean",x)]
 	probNorm <- as.numeric(sub(".*?probNorm.*?(\\d+(?:\\.\\d+)?).*", "\\1", x))
-	startDate <- as.numeric(sub(".*?start.*?(\\d+(?:\\.\\d+)?).*", "\\1", x))
+	pattern <- "start:\\s*(.*?)\\s*,resolution"
+	startDate <- as.numeric(regmatches(x, regexec(pattern, x))[[1]][2])
 	resolution <- as.numeric(sub(".*?resolution.*?(\\d+(?:\\.\\d+)?).*", "\\1", x))
 
         x <- sub('.*prob_sigma:', '', x)
@@ -15,14 +14,12 @@ read_kde <- function(x,sigma=1)
 	x <- unlist(strsplit(x,',')) |> as.numeric()
 	x.mat <- t(matrix(x,nrow=2)) |> as.data.frame()
 	colnames(x.mat) <- c('raw.mean','raw.sd')
-	x.mat$CalBP <- seq(from=startDate,by=-resolution,length.out=nrow(x.mat))
+	x.mat$BCAD <- seq(from=startDate,by=resolution,length.out=nrow(x.mat))
+	x.mat$calBP <- abs(x.mat$BCAD - 1950)
 	x.mat$hi <- (x.mat$raw.mean + x.mat$raw.sd * sigma) * probNorm
 	x.mat$lo <- (x.mat$raw.mean - x.mat$raw.sd * sigma) * probNorm
 	x.mat$lo[which(x.mat$lo<0)] <- 0
 	x.mat$m <- x.mat$raw.mean * probNorm
-# 	plot(x.mat$CalBP,x.mat$m,type='n',xlim=rev(range(x.mat$CalBP)))
-# 	polygon(c(x.mat$CalBP,rev(x.mat$CalBP)),c(x.mat$lo,rev(x.mat$hi)),border=NA,col=rgb(0,0,1,0.2))
-# 	lines(x.mat$CalBP,x.mat$m,type='l',xlim=rev(range(x.mat$CalBP)))
 	return(x.mat)
 }
 
