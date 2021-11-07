44d2f807fe0e155509c9130fcc2a0e20f4b7419d
E.R. Crema <enrico.crema@gmail.com>
2021-11-07 17:18:51 +0000

    Added full ADMUR results

--- runscripts/admur.R
+++ runscripts/admur.R
@@ -19,6 +19,10 @@ PD.small <- phaseCalibrator(data.small, CalArray.small, remove.external = TRUE)
 exp.large <- JDEoptim(lower=c(0), upper=c(1),fn=objectiveFunction, PDarray=PD.large, type='exp', NP=20, trace=T)
 exp.small <- JDEoptim(lower=c(0), upper=c(1),fn=objectiveFunction, PDarray=PD.small, type='exp', NP=20, trace=T)
 
-chain.large <- mcmc(PDarray=PD.large, startPars=exp.large$par, type='exp', N=100000, burn=2000, thin=5, jumps =0.025)
-chain.small <- mcmc(PDarray=PD.small, startPars=exp.small$par, type='exp', N=100000, burn=2000, thin=5, jumps =0.01)
+chain.large <- mcmc(PDarray=PD.large, startPars=exp.large$par, type='exp', N=100000, burn=2000, thin=5, jumps =0.0004)
+chain.small <- mcmc(PDarray=PD.small, startPars=exp.small$par, type='exp', N=100000, burn=2000, thin=5, jumps =0.0004)
+
+save(exp.large,exp.small,chain.large,chain.small,file=here('results','admur_res.RData'))
+
+
 
