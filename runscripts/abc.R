00f158f2fd93b32e2d1d108286130f47915d8dbd
Enrico R. Crema <enrico.crema@gmai.com>
2021-11-08 08:51:22 +0000

    Updated ABC routine
    Given the same priors for large and small sample the main abc function
    was updated to allow for multiple comparisons and a single
    uncalibration.

--- runscripts/abc.R
+++ runscripts/abc.R
@@ -9,11 +9,18 @@ load(here('data','sim2.RData'))
 # Source abc function
 source(here('src','abc_sim_exp.R'))
 
+# Create vector with number of samples
+nn <- c(length(cra.large),length(cra.small))
+
 # Calibrate and generate target SPD
 cal.large <- calibrate(cra.large,cra.error.large)
 cal.small <- calibrate(cra.small,cra.error.small)
 spd.large <- spd(cal.large,timeRange=c(6500,4500),spdnormalised=T)
 spd.small <- spd(cal.small,timeRange=c(6500,4500),spdnormalised=T)
+# Extract normalised SPD values
+spd.large.dens <- spd.large$grid$PrDens
+spd.small.dens <- spd.small$grid$PrDens
+spd.target.dens <- list(spd.large.dens,spd.small.dens)
 
 # Generate priors
 nsim = 1000000
@@ -31,9 +38,8 @@ opts <- list(progress = progress)
 res <- foreach (i=1:nsim,.packages=c('rcarbon'),.options.snow = opts) %dopar%
 {
 	set.seed(i)
-	res.large  <- abc_sim_exp(r[i],n=length(cra.large),a=6500,b=4500,errors=cra.error.large,target.spd=spd.large)
-	res.small  <- abc_sim_exp(r[i],n=length(cra.small),a=6500,b=4500,errors=cra.error.small,target.spd=spd.small)
-	return(c(res.large,res.small))
+	res  <- abc_sim_exp(r[i],n=nn,a=6500,b=4500,errors=cra.error.large,target.spd=spd.target.dens)
+	return(res)
 }
 
 res.abc = unlist(res) |> matrix(nrow=2) |> t() |> as.data.frame()
@@ -41,11 +47,3 @@ colnames(res.abc) <- c('epsilon.large','epsilon.small')
 res.abc$r = r
 
 save(res.abc,file=here('results','abc_res.RData'))
-
-
-
-
-
-
-
-
--- src/abc_sim_exp.R
+++ src/abc_sim_exp.R
@@ -1,3 +1,13 @@
+
+# Generates candidate SPDs and comapre them to user provided target SPDs
+# r ... exponential growth rate
+# n ... number of samples
+# a ... start of window of analysis (in cal BP)
+# b ... end of window of analyses (in cal BP)
+# errors ... vector from which 14C errors are sampled from
+# target.spd ... target spd. Vector of normalised SPD values
+# NOTE: n and target.spd can be provided by a vector and a list, in which case epsilon will be calculated for matching indexes.
+
 abc_sim_exp <- function(r,n,a,b,errors,target.spd)
 {
 	require(rcarbon)
@@ -7,9 +17,15 @@ abc_sim_exp <- function(r,n,a,b,errors,target.spd)
 	model <- data.frame(CalBP=a:b,PrDens = pop/sum(pop))
 	class(model) <- 'CalGrid'
 	cra.model <- uncalibrate(model,verbose=F)
-	uncal.samples = sample(cra.model$CRA,size=n,prob=cra.model$PrDens,replace=TRUE)
-	cal.samples = calibrate(uncal.samples,sample(errors,size=n,replace=T),verbose=FALSE)
-	spd.candidate = spd(cal.samples,timeRange=c(a,b),verbose=FALSE,spdnormalised=T)
-	epsilon = sqrt(sum((target.spd$grid$PrDens-spd.candidate$grid$PrDens)^2))
+
+	niter  <- length(n)
+	epsilon  <- numeric(length=niter)
+	for (i in 1:niter)
+	{
+		uncal.samples = sample(cra.model$CRA,size=n[i],prob=cra.model$PrDens,replace=TRUE)
+		cal.samples = calibrate(uncal.samples,sample(errors,size=n[i],replace=T),verbose=FALSE)
+		spd.candidate = spd(cal.samples,timeRange=c(a,b),verbose=FALSE,spdnormalised=T)
+		epsilon[i] = sqrt(sum((target.spd[[i]]-spd.candidate$grid$PrDens)^2))
+	}
 	return(epsilon)
 }
