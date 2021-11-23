6e0080607eff9baf128266f1ae1d1ad5c159eed7
Enrico R. Crema <enrico.crema@gmai.com>
2021-11-23 17:11:34 +0000

    Updated script for bootstrapped CKDE with tiny sim

--- runscripts/baydem_gauss_mixture.R
+++ runscripts/baydem_gauss_mixture.R
@@ -1,6 +1,31 @@
 library(here)
 library(baydem)
 
+# Tiny Sample ----
+tiny.data <- import_rc_data(here('data','tiny_sim1_sample.csv'),phi_m_col='phi_m',sig_m_col='sig_m')
+set_rc_meas(here('results'),"baydem_tiny_res",tiny.data)
+analysis <- readRDS(here('results','baydem_tiny_res.rds'))
+tau_range <- calc_tau_range(analysis$rc_meas,calibration_curve="intcal20",dtau=5)
+density_model <- list(type="trunc_gauss_mix", tau_min=tau_range$tau_min,tau_max=tau_range$tau_max,K=2:20)
+set_density_model(here('results'),"baydem_tiny_res",density_model)
+set_calib_curve(here('results'),"baydem_tiny_res","intcal20")
+analysis <- readRDS(here('results','baydem_tiny_res.rds'))
+hp <- list()
+# Parameter for the dirichlet draw of the mixture probabilities
+hp$alpha_d <- 1 
+# The gamma distribution shape parameter for sigma
+hp$alpha_s <- 5
+# The gamma distribution rate parameter for sigma, yielding a mode of 50
+hp$alpha_r <- (5 - 1) / 500
+# Spacing for the measurement matrix (years)
+hp$dtau <- 1
+
+# Run Bayesian Analyses in Stan
+do_bayesian_inference(here('results'),"baydem_tiny_res",hp,control=list(samps_per_chain=5000))
+
+# Bayesian Summary
+do_bayesian_summary(here('results'),"baydem_tiny_res")
+
 # Small Sample ----
 small.data <- import_rc_data(here('data','small_sim1_sample.csv'),phi_m_col='phi_m',sig_m_col='sig_m')
 set_rc_meas(here('results'),"baydem_small_res",small.data)
@@ -54,6 +79,16 @@ do_bayesian_inference(here('results'),"baydem_large_res",hp,control=list(samps_p
 do_bayesian_summary(here('results'),"baydem_large_res")
 
 # Exctract best fitted model estimates ----
+baydem.tiny <- readRDS(here('results','baydem_tiny_res.rds'))
+bestK.tiny <- baydem.tiny$K_best
+baydem.tiny.plot <- data.frame(CalBP=BCADtoBP(baydem.tiny$bayesian_summary$tau))
+ind.lo.tiny <- which(baydem.tiny$bayesian_summary$probs == 0.025)
+ind.hi.tiny <- which(baydem.tiny$bayesian_summary$probs == 0.975)
+ind.m.tiny <- which(baydem.tiny$bayesian_summary$probs == 0.5)
+baydem.tiny.plot$lo <- baydem.tiny$bayesian_summary$Qdens[ind.lo.tiny,]
+baydem.tiny.plot$hi <- baydem.tiny$bayesian_summary$Qdens[ind.hi.tiny,]
+baydem.tiny.plot$m <- baydem.tiny$bayesian_summary$Qdens[ind.m.tiny,]
+
 baydem.small <- readRDS(here('results','baydem_small_res.rds'))
 bestK.small <- baydem.small$K_best
 baydem.small.plot <- data.frame(CalBP=BCADtoBP(baydem.small$bayesian_summary$tau))
@@ -74,7 +109,7 @@ baydem.large.plot$lo <- baydem.large$bayesian_summary$Qdens[ind.lo.large,]
 baydem.large.plot$hi <- baydem.large$bayesian_summary$Qdens[ind.hi.large,]
 baydem.large.plot$m <- baydem.large$bayesian_summary$Qdens[ind.m.large,]
 
-save(bestK.small,bestK.large,baydem.small.plot,baydem.large.plot,file=here('results','baydem_res.RData'))
+save(bestK.tiny,bestK.small,bestK.large,baydem.tiny.plot,baydem.small.plot,baydem.large.plot,file=here('results','baydem_res.RData'))
 
 
 
--- runscripts/bootstrapped_ckde.R
+++ runscripts/bootstrapped_ckde.R
@@ -4,17 +4,21 @@ library(here)
 # Read Data ----
 large <- read.csv(file=here('data','large_sim1_sample.csv'))
 small <- read.csv(file=here('data','small_sim1_sample.csv'))
+tiny <- read.csv(file=here('data','tiny_sim1_sample.csv'))
 sim <- read.csv(file=here('data','sim1.csv'))
 # Calibrate ----
 cal.large <- calibrate(large$cra,large$cra.error)
 cal.small <- calibrate(small$cra,small$cra.error)
+cal.tiny <- calibrate(tiny$cra,tiny$cra.error)
 
 # Bootstrapping ----
 boot.large <- sampleDates(cal.large,nsim=1000,boot=TRUE)
 boot.small <- sampleDates(cal.small,nsim=1000,boot=TRUE)
+boot.tiny <- sampleDates(cal.tiny,nsim=1000,boot=TRUE)
 
 # Composite KDE ---
 ckde.large <- ckde(boot.large,timeRange=c(7000,3000),bw=50)
 ckde.small <- ckde(boot.small,timeRange=c(7000,3000),bw=50)
+ckde.tiny <- ckde(boot.tiny,timeRange=c(7000,3000),bw=50)
 
-save(ckde.large,ckde.small,file=here('results','ckde_res.RData'))
+save(ckde.large,ckde.small,ckde.tiny,file=here('results','ckde_res.RData'))
